25c6d1e40e2f7dc296442bf6f01cdbc0ef51ebbd
= <=>
2025-12-02 13:10:44 +0700

    update experience

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -5,16 +5,35 @@
         <div class="mb-4 grid grid-cols-2 gap-4">
             <div>
                 <div class="mb-1 font-semibold">Search</div>
-                <x-text-input placeholder="Search for any text" name="search" value="" />
+                <x-text-input placeholder="Search for any text" name="search" value="{{ request('search') }}" />
             </div>
             <div>
                 <div class="mb-1 font-semibold">Salary</div>
                 <div class="flex space-x-2">
-                    <x-text-input placeholder="from" name="min_salary" value="" />
-                    <x-text-input placeholder="to" name="max_salary" value="" />
+                    <x-text-input placeholder="from" name="min_salary" value="{{ request('min_salary') }}" />
+                    <x-text-input placeholder="to" name="max_salary" value="{{ request('max_salary') }}" />
                 </div>
             </div>
-            <div>3</div>
+            <div>
+                <div class="mb-1 font-semibold">Experience</div>
+
+                <label for="experience" class="mb-1 flex items-center">
+                    <input type="radio" name="experience" value="" @checked(!request('experience'))/>
+                    <span class="ml-2">All</span>
+                </label>
+                <label for="experience" class="mb-1 flex items-center">
+                    <input type="radio" name="experience" value="entry" @checked('entry' === request('experience'))/>
+                    <span class="ml-2">Entry</span>
+                </label>
+                <label for="experience" class="mb-1 flex items-center">
+                    <input type="radio" name="experience" value="intermediate" @checked('intermediate' === request('experience')) />
+                    <span class="ml-2">Intermediate</span>
+                </label>
+                <label for="experience" class="mb-1 flex items-center">
+                    <input type="radio" name="experience" value="senior" @checked('senior' === request('experience')) />
+                    <span class="ml-2">Senior</span>
+                </label>
+            </div>
             <div>4</div>
         </div>
 
